bfc1347535b9fc65e005d19e782919aff7169cb1
Mustafa Türkan <mustafatrkn93@outlook.com>
2017-06-17 14:54:17 +0300

    yeni classlar eklendi ve class bağlantıları yapıldı

--- DAL/Model/Lokantalar.cs
+++ DAL/Model/Lokantalar.cs
@@ -10,7 +10,18 @@ namespace DAL.Model
         public int LokantaID { get; set; }
         public string LokantaAdi { get; set; }
         public string LokantaAdresi { get; set; }
-        public string TanitimAdresi {get;set;}
+        public string Tanitim {get;set;}
         public int MasaSayiyi { get; set; }
+
+
+        //Bağlantılar
+        //Bir lokantanın birden çok menüsü olacak
+        public virtual List<SogukIcecek> SogukIcecekler {get;set;}
+        public virtual List<SicakIcecekler> SicakİcecekLer { get; set; }
+        public virtual List<Tatlilar>Tatlilar{get;set;}
+        public virtual List<Yiyecekler>Yiyecekler{ get; set;}
+        public virtual List<Menuler> Menuler {get;set;}
+        public virtual Kullanici Kullanici { get; set;}
+        public virtual List<Masa> Masalar { get;set;}
     }
 }
---
+++ DAL/Model/İstatistik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAL.Model
+{
+    public class İstatistik
+    {
+        public int İstatistikID { get; set; }
+        public int GunlukHasilat { get; set; }
+        public int AylikHasilat { get; set; }
+
+        public int YillikHasilat{ get; set;}
+
+        public int GenelHasilat{ get; set;}
+
+        public string EnCokTercihEdilenSogukİcecek { get; set; }
+        public string EnCokTercihEdilenSicakİcecek { get; set; }
+        public string EnCokTercihEdilenTatli{ get; set; }
+        public string EnCokTercihEdilenYiyecek{ get; set; }
+        public string EnCokTercihEdilenMenu{ get; set; }
+
+        public virtual Lokantalar lokanta { get; set; }
+
+    }
+}
