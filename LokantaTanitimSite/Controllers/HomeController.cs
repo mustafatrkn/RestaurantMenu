aca0c18bbd7d57c256d3a911b4e5b3723eb24616
Mustafa Türkan <mustafatrkn93@outlook.com>
2017-06-20 17:34:59 +0300

    E Mail Gönderme işlemi yapıldı

--- LokantaTanitimSite/Controllers/HomeController.cs
+++ LokantaTanitimSite/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,10 +12,44 @@ namespace LokantaTanitimSite.Controllers
 
         public ActionResult AnaSayfa()
         {
-
-
-
             return View();
         }
+
+        public void sendMessage(string name, string message, string eposta)
+        {
+            MailMessage mesajim = new MailMessage();
+            SmtpClient istemci = new SmtpClient();
+            istemci.Credentials = new System.Net.NetworkCredential("mustafatrkn93@outlook.com", "haskovo35");
+            istemci.Port = 587;
+            istemci.Host="smtp.live.com";
+            istemci.EnableSsl = true;
+            mesajim.To.Add("mustafatrkn93@outlook.com");
+            mesajim.From=new MailAddress(eposta);
+            mesajim.Subject="Müşteri Bilgilendirme";
+            mesajim.Body=message;
+            istemci.Send(mesajim);
+
+        }
+
+        [HttpPost]
+        public JsonResult PostMessage(string name, string message, string eposta)
+        {
+            var deger = "";
+            if (eposta !=null)
+            {                
+                sendMessage(name, message, eposta);
+                deger = "1";
+            }
+            else
+            {
+                deger = "0";
+            }
+
+
+            return Json(deger);
+        }
+
+
+
     }
 }
--- LokantaTanitimSite/Scripts/Mayki.js
+++ LokantaTanitimSite/Scripts/Mayki.js
@@ -1,1 +1,19 @@
-﻿
+﻿$(document).ready(function () {
+    $('#submit').click(function () {
+        alert($('#name').val())
+        $.post("http://localhost:59591/Home/PostMessage", {
+            name: $('#name').val(),
+            message: $('#comment').val(),
+            eposta: $('#email').val()
+        }, function (data, status) {
+            if (data == "1") {
+                //popup mesajınız gönderildi
+            }
+            else
+            {
+                //popup mesajı gönderildi
+            }
+        })
+
+    })
+})
